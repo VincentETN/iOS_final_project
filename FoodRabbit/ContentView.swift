c831117bbb7a66828c26a08de6fa7fa26f6719e5
謝昀羲 <vincent@xieyunxideMac.local>
2021-06-06 15:03:41 +0800

    init

---
+++ FoodRabbit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodRabbit
+//
+//  Created by 謝昀羲 on 2021/6/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodRabbit/FoodRabbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodRabbitApp.swift
+//  FoodRabbit
+//
+//  Created by 謝昀羲 on 2021/6/6.
+//
+
+import SwiftUI
+
+@main
+struct FoodRabbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
